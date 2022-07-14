9fb65ce4ab2d720303626cefa2ec2a518588d8c7
ZhiganovD <zhiganov.dv@phystech.edu>
2022-07-15 02:22:20 +0300

    Gates again

--- gates.py
+++ gates.py
@@ -5,3 +5,9 @@ H = QGate(np.array([[1, 1], [1, -1]]) / np.sqrt(2))
 X = QGate([[0, 1], [1, 0]])
 Y = QGate([[0, -1j], [1j, 0]])
 Z = QGate([[1, 0], [0, -1]])
+T = QGate([[0, 0], [0, (1 + 1j) / np.sqrt(2)]])
+def ry(state, phi):
+    return np.array([
+        [np.cos(phi / 2), -np.sin(phi / 2)],
+        [np.sin(phi / 2),  np.cos(phi / 2)]
+    ]) @ state
