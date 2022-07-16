8f3cdaa7940e814442c832981ca3e4a32eed4c98
ZhiganovD <zhiganov.dv@phystech.edu>
2022-07-16 18:09:17 +0300

    QNN

--- main.py
+++ main.py
@@ -1,15 +1,109 @@
-from quantum import *
-from gates import *
-
 import pennylane as qml
 from pennylane import numpy as np
+from pennylane.templates import RandomLayers
 
-dev = qml.device("default.qubit", wires=2)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+#%config InlineBackend.figure_format = 'retina'
+
+n_epochs = 20   # Количество эпох обучения
+n_layers = 1    # Число случайных квантовых блоков
+n_wires = 4     # Число выходных каналов после квантовых блоков
+n_train = 20    # Размер тренировочного датасета
+n_test = 10     # Размер тестового датасета
+
+# Инициализация генераторов случайных чисел
+np.random.seed(0)  
+tf.random.set_seed(255)  
+
+mnist_dataset = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist_dataset.load_data()
+
+# Ограничение размера датасета
+train_images = train_images[:n_train]
+train_labels = train_labels[:n_train]
+test_images = test_images[:n_test]
+test_labels = test_labels[:n_test]
+
+# Нормализация изображений из диапазона (0, 255) в (0, 1)
+train_images = train_images / 255
+test_images = test_images / 255
+
+# Добавление дополнительной размерности к данным для сверточных каналов
+train_images = np.array(train_images[..., tf.newaxis], requires_grad=False)
+test_images = np.array(test_images[..., tf.newaxis], requires_grad=False)
+
+dev = qml.device("default.qubit", wires=n_wires)
+
+# Генерация значений параметров для квантовых слоев
+rand_params = np.random.uniform(high=2 * np.pi, size=(n_layers, n_wires))
 
 @qml.qnode(dev)
-def circuit(param):
-    qml.RX(param, wires=0)
-    qml.CNOT(wires=[0, 1])
-    return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))
+def circuit(phi):
+    # Кодирование 4 классических входных данных
+    for j in range(n_wires):
+        qml.RY(np.pi * phi[j], wires=j)
 
-print(circuit(np.pi / 2))
+    # Случайная квантовая цепь
+    RandomLayers(rand_params, wires=list(range(n_wires)))
+
+    # Измерения, которые дают 4 классических выходных значений для следующих слоев
+    return [qml.expval(qml.PauliZ(j)) for j in range(n_wires)]
+
+def quanv(image):
+    """Функция квантовой свертки над входным изображением."""
+
+    out = np.zeros((14, 14, n_wires))
+
+    # Циклы по координатам верхнего левого пикселя блоков 2х2
+    for j in range(0, 28, 2):
+        for k in range(0, 28, 2):
+            # Обработка блока 2x2 из изображения квантовой цепью
+            q_results = circuit(
+                [
+                    image[j, k, 0],
+                    image[j, k + 1, 0],
+                    image[j + 1, k, 0],
+                    image[j + 1, k + 1, 0]
+                ]
+            )
+            # Запись результатов наблюдения в выходной пиксель (j/2, k/2)
+            for c in range(n_wires):
+                out[j // 2, k // 2, c] = q_results[c]
+    return out
+
+q_train_images = []
+for idx, img in enumerate(train_images):
+    q_train_images.append(quanv(img))
+q_train_images = np.asarray(q_train_images)
+print("Препроцессинг тренировочных изображений квантовой сверткой выполнен.")
+
+q_test_images = []
+for idx, img in enumerate(test_images):
+    q_test_images.append(quanv(img))
+q_test_images = np.asarray(q_test_images)
+print("Препроцессинг тестовых изображений квантовой сверткой выполнен.")
+
+n_samples = 4
+n_channels = n_wires
+fig, axes = plt.subplots(1 + n_channels, n_samples, figsize=(10, 10))
+
+for k in range(n_samples):
+    axes[0, 0].set_ylabel("Input")
+    if k != 0:
+        axes[0, k].yaxis.set_visible(False)
+    axes[0, k].imshow(train_images[k, :, :, 0], cmap="gray")
+
+    # Отрисовка
+    for c in range(n_channels):
+        axes[c + 1, 0].set_ylabel(f"Output [ch. {c}]")
+        if k != 0:
+            axes[c, k].yaxis.set_visible(False)
+        axes[c + 1, k].imshow(q_train_images[k, :, :, c], cmap="gray")
+
+plt.tight_layout()
+plt.show()
